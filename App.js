8ea8bb6128e261519e6fcd659a1d66af5781ffb9
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-02-01 11:23:35 +0900

    webview内との連携

--- App.js
+++ App.js
@@ -1,39 +1,54 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, WebView } from 'react-native';
+import { Platform, StyleSheet, Text, View, WebView } from 'react-native';
 
-const uri = 'http://localhost:3000';
+const uri = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+const jsCode = `
+var inputFile = document.getElementById('input-file');
+inputFile.setAttribute('disabled', true);
+var fileSelectButton = document.getElementById('file-select');
+fileSelectButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.postMessage('ShowCamera');
+});
+`;
 
-export default class App extends Component<> {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { cameraView: false };
   }
 
-  jsCode = () =>
-    `
-    const fileSelectButton = document.getElementById('file-select');
-    fileSelectButton.addEventListener('click', function(e) {
-      e.preventDefault();
-      alert();
-      this.setState({ cameraView: true });
-    });
-    `;
+  onMessage = (e) => {
+    const { data } = e.nativeEvent;
+    if (data === 'ShowCamera') {
+      this.showCamera();
+    }
+  };
+
+  showCamera = () => this.setState({ cameraView: true });
+
+  showCamera = () => this.setState({ cameraView: true });
 
   render() {
     return (
       <View style={styles.container}>
         {this.state.cameraView ? (
-          <View>
+          <View style={{ flex: 1, backgroundColor: 'green' }}>
             <Text>カメラ画面</Text>
           </View>
         ) : (
-          <WebView source={{ uri }} injectedJavaScript={this.jsCode()} />
+          <WebView
+            source={{ uri }}
+            onMessage={this.onMessage}
+            injectedJavaScript={jsCode}
+            javaScriptEnabledAndroid
+          />
         )}
       </View>
     );
